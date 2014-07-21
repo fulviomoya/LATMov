ecd02f68e314c120674e8b4dc747a94184e528bc
fulviomoya <fulvio.127@gmail.com>
2014-07-20 20:02:48 -0400

    first upload all files

---
+++ LATMov/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LATMov.Startup))]
+namespace LATMov
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
