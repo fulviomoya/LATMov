c427807a88371d4bf776c4014ba78976bd44efbe
fulviomoya <fulvio.127@gmail.com>
2014-07-31 21:02:48 -0400

    Adding all models to DbContext class

--- LATMov.Persistence/LATMovDbContext.cs
+++ LATMov.Persistence/LATMovDbContext.cs
@@ -11,8 +11,17 @@ namespace LATMov.Persistence {
         public LATMovDbContext() { 
             
         }
+        #region DbSets Models
+        public DbSet<Award> Awards { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+        public DbSet<FilmStudio> FilmStudios { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Movie> Movies { get; set; }
 
-        public DbSet<Movie> movies { get; set; }
-        //DbSet<AwardModels> Award { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<SocialNetworkAccount> SocialNetworkAccounts { get; set; }
+        public DbSet<SoundTrack> SoundTracks { get; set; }
+        public DbSet<Trailler> Traillers { get; set; }
+        #endregion
     }
 }
