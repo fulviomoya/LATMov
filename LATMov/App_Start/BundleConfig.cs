47101b06de59a41cbb8c714fa5e68f34a4f3a762
fulviomoya <fulvio.127@gmail.com>
2014-08-05 00:17:23 -0400

    adding a new template layout and JavaScripts libraries

--- LATMov/App_Start/BundleConfig.cs
+++ LATMov/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace LATMov
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css", 
+                      "~/Content/screen.css", 
+                      "~/Content/formalize.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
